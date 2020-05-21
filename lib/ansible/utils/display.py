2020663feb6d224b42e04f793e1b921ffbd2e396
Matt Davis <mrd@redhat.com>
2020-05-21 16:53:36 -0700

    add defensive conversions to display methods, tests
    * prevent display/debug/verbose messages from causing new failures (eg by increasing verbosity to a level that tries to render a bytes-ish message string)
    * defensive early convert messages and other strings to text
    * adds basic regression tests with mixed bytes/text non-ASCII inputs (that fail without the changes)

--- lib/ansible/utils/display.py
+++ lib/ansible/utils/display.py
@@ -150,10 +150,9 @@ class Display(with_metaclass(Singleton, object)):
                     self.b_cowsay = b_cow_path
 
     def display(self, msg, color=None, stderr=False, screen_only=False, log_only=False, newline=True):
-        """ Display a message to the user
-
-        Note: msg *must* be a unicode string to prevent UnicodeError tracebacks.
-        """
+        """ Display a message to the user """
+        # defensive conversion to prevent display/logging code from bombing
+        msg = to_text(msg)
 
         nocolor = msg
 
@@ -235,19 +234,26 @@ class Display(with_metaclass(Singleton, object)):
 
     def debug(self, msg, host=None):
         if C.DEFAULT_DEBUG:
+            msg = to_text(msg)
+            if host:
+                host = to_text(host)
+
             if host is None:
                 self.display("%6d %0.5f: %s" % (os.getpid(), time.time(), msg), color=C.COLOR_DEBUG)
             else:
                 self.display("%6d %0.5f [%s]: %s" % (os.getpid(), time.time(), host, msg), color=C.COLOR_DEBUG)
 
     def verbose(self, msg, host=None, caplevel=2):
+        msg = to_text(msg)
+        if host:
+            host = to_text(host)
 
         to_stderr = C.VERBOSE_TO_STDERR
         if self.verbosity > caplevel:
             if host is None:
                 self.display(msg, color=C.COLOR_VERBOSE, stderr=to_stderr)
             else:
-                self.display("<%s> %s" % (host, msg), color=C.COLOR_VERBOSE, stderr=to_stderr)
+                self.display(u"<%s> %s" % (host, msg), color=C.COLOR_VERBOSE, stderr=to_stderr)
 
     def deprecated(self, msg, version=None, removed=False):
         ''' used to print out a deprecation message.'''
@@ -272,13 +278,13 @@ class Display(with_metaclass(Singleton, object)):
             self._deprecations[new_msg] = 1
 
     def warning(self, msg, formatted=False):
-
+        msg = to_text(msg)
         if not formatted:
-            new_msg = "[WARNING]: %s" % msg
+            new_msg = u"[WARNING]: %s" % msg
             wrapped = textwrap.wrap(new_msg, self.columns)
-            new_msg = "\n".join(wrapped) + "\n"
+            new_msg = u"\n".join(wrapped) + u"\n"
         else:
-            new_msg = "\n[WARNING]: \n%s" % msg
+            new_msg = u"\n[WARNING]: \n%s" % msg
 
         if new_msg not in self._warns:
             self.display(new_msg, color=C.COLOR_WARN, stderr=True)
@@ -292,6 +298,7 @@ class Display(with_metaclass(Singleton, object)):
         '''
         Prints a header-looking line with cowsay or stars with length depending on terminal width (3 minimum)
         '''
+        msg = to_text(msg)
         if self.b_cowsay and cows:
             try:
                 self.banner_cowsay(msg)
@@ -307,6 +314,7 @@ class Display(with_metaclass(Singleton, object)):
         self.display(u"\n%s %s" % (msg, stars), color=color)
 
     def banner_cowsay(self, msg, color=None):
+        msg = to_text(msg)
         if u": [" in msg:
             msg = msg.replace(u"[", u"")
             if msg.endswith(u"]"):
@@ -324,6 +332,7 @@ class Display(with_metaclass(Singleton, object)):
         self.display(u"%s\n" % to_text(out), color=color)
 
     def error(self, msg, wrap_text=True):
+        msg = to_text(msg)
         if wrap_text:
             new_msg = u"\n[ERROR]: %s" % msg
             wrapped = textwrap.wrap(new_msg, self.columns)
@@ -348,6 +357,11 @@ class Display(with_metaclass(Singleton, object)):
             return input(prompt_string)
 
     def do_var_prompt(self, varname, private=True, prompt=None, encrypt=None, confirm=False, salt_size=None, salt=None, default=None, unsafe=None):
+        varname = to_text(varname)
+        if prompt:
+            prompt = to_text(prompt)
+        if default:
+            default = to_text(default)
 
         result = None
         if sys.__stdin__.isatty():
@@ -355,16 +369,16 @@ class Display(with_metaclass(Singleton, object)):
             do_prompt = self.prompt
 
             if prompt and default is not None:
-                msg = "%s [%s]: " % (prompt, default)
+                msg = u"%s [%s]: " % (prompt, default)
             elif prompt:
-                msg = "%s: " % prompt
+                msg = u"%s: " % prompt
             else:
-                msg = 'input for %s: ' % varname
+                msg = u'input for %s: ' % varname
 
             if confirm:
                 while True:
                     result = do_prompt(msg, private)
-                    second = do_prompt("confirm " + msg, private)
+                    second = do_prompt(u"confirm " + msg, private)
                     if result == second:
                         break
                     self.display("***** VALUES ENTERED DO NOT MATCH ****")
