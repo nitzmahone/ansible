df90df81021f6da2850cb266ef3b6cc647a1cc02
Matt Davis <mrd@redhat.com>
2023-10-20 16:53:22 -0700

    remove dead unit test bits

--- test/units/utils/test_vars.py
+++ test/units/utils/test_vars.py
@@ -25,7 +25,6 @@ from unittest import mock
 import unittest
 from ansible.errors import AnsibleError
 from ansible.utils.vars import combine_vars, merge_hash
-from ansible.vars.manager import VarsWithSources
 
 
 class TestVariableUtils(unittest.TestCase):
@@ -39,11 +38,6 @@ class TestVariableUtils(unittest.TestCase):
         dict(
             a=dict(a=1),
             b=dict(b=2),
-            result=dict(a=1, b=2),
-        ),
-        dict(
-            a=dict(a=1),
-            b=VarsWithSources().new_vars_with_sources(dict(b=2), dict(b='task vars')),
             result=dict(a=1, b=2),
         ),
         dict(
@@ -62,11 +56,6 @@ class TestVariableUtils(unittest.TestCase):
             a=dict(a=1),
             b=dict(b=2),
             result=dict(a=1, b=2)
-        ),
-        dict(
-            a=dict(a=1),
-            b=VarsWithSources().new_vars_with_sources(dict(b=2), dict(b='task vars')),
-            result=dict(a=1, b=2),
         ),
         dict(
             a=dict(a=1, c=dict(foo='bar')),
