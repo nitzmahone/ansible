c4e72694d85f4fd7a71f93e17db0a2f312ae2147
Matt Clay <matt@mystile.com>
2024-01-19 11:37:48 -0800

    Remove obsolete uses of ANSIBLE_JINJA2_NATIVE env var

--- test/integration/targets/template_jinja2_non_native/runme.sh
+++ test/integration/targets/template_jinja2_non_native/runme.sh
@@ -2,10 +2,6 @@
 
 set -eux
 
-# since the template module itself always uses non-native Jinja by default, turn it on globally, then let the test
-# diddle as needed
-export ANSIBLE_JINJA2_NATIVE=1
 ansible-playbook 46169.yml -v "$@"
 python -m pip install "Jinja2>=3.1.0"
 ansible-playbook macro_override.yml -v "$@"
-unset ANSIBLE_JINJA2_NATIVE
