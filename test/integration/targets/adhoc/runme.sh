ba2119a26b392cc83703feca98cf6a008eb6f1d7
Matt Davis <nitzmahone@redhat.com>
2024-07-05 15:03:22 -0700

    quoting sanity fix

--- test/integration/targets/adhoc/runme.sh
+++ test/integration/targets/adhoc/runme.sh
@@ -9,6 +9,6 @@ ansible -a 'sleep 20' --task-timeout 5 localhost |grep 'The command action faile
 ansible --task-timeout 5 localhost -m command -a '{"cmd": "whoami"}' | grep 'rc=0'
 
 # ensure that legacy deserializer behaves as expected on JSON CLI args (https://github.com/ansible/ansible/issues/82600)
-ansible localhost -m debug -a var=fromcli -e '{"fromcli":{"__ansible_unsafe":"{{\"hello\"}}"}}' > "$OUTPUT_DIR/output.txt" 2>&1
-grep '"fromcli": "{{."hello."}}"' $OUTPUT_DIR/output.txt  # ensure that the template was not rendered
-grep "Skipped untrusted template" $OUTPUT_DIR/output.txt  # look for the untrusted template warning text
+ansible localhost -m debug -a var=fromcli -e '{"fromcli":{"__ansible_unsafe":"{{\"hello\"}}"}}' > "${OUTPUT_DIR}/output.txt" 2>&1
+grep '"fromcli": "{{."hello."}}"' "${OUTPUT_DIR}/output.txt"  # ensure that the template was not rendered
+grep "Skipped untrusted template" "${OUTPUT_DIR}/output.txt"  # look for the untrusted template warning text
