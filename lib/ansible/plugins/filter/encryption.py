d3f6c843ba418d51ce860690119a0838ac995dcc
Matt Davis <nitzmahone@redhat.com>
2026-08-07 17:07:19 -0700

    WIP vault revamp rebase on 2.22 devel

--- lib/ansible/plugins/filter/encryption.py
+++ lib/ansible/plugins/filter/encryption.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from ansible import constants as C
 from ansible.errors import AnsibleError
 from ansible.module_utils.common.text.converters import to_native, to_bytes
 from ansible._internal._templating._jinja_common import VaultExceptionMarker
@@ -13,17 +14,22 @@ from ansible.utils.display import Display
 display = Display()
 
 
-def do_vault(data, secret, salt=None, vault_id='filter_default', wrap_object=False):
+def do_vault(data, secret, salt=None, vault_id='filter_default', wrap_object=False, vaultid=None, method_name=None):
     if not isinstance(secret, (str, bytes)):
         raise TypeError(f"Secret passed is required to be a string, instead we got {type(secret)}.")
 
     if not isinstance(data, (str, bytes)):
         raise TypeError(f"Can only vault strings, instead we got {type(data)}.")
 
+    if method_name is not None:
+        choices = C.config.get_config_choices('VAULT_METHOD')
+        if method_name not in choices:
+            raise TypeError(f"Invalid vault encryption method {method_name!r}, valid choices are: {', '.join(choices)}")
+
     vs = VaultSecret(to_bytes(secret))
     vl = VaultLib()
     try:
-        vault = vl.encrypt(to_bytes(data), vs, vault_id, salt)
+        vault = vl.encrypt(to_bytes(data), vs, vault_id, salt, method_name=method_name)
     except Exception as ex:
         raise AnsibleError("Unable to encrypt.") from ex
 
